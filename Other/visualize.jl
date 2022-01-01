0c25eda7c0e0eb26ed36d46a63738ab3c84d9b0d
egliette <leminhtu74@gmail.com>
2022-01-01 22:56:16 +0700

    Tree Update

--- Other/visualize.jl
+++ Other/visualize.jl
@@ -3,6 +3,7 @@ using D3Trees
 number = Vector([1])
 children = Vector{Vector}()
 text = Vector{String}()
+style = Vector{String}()
 
 # Action
 FEED = 1
@@ -15,31 +16,38 @@ CRYING = true
 QUIET = false
 Obs = Dict(CRYING => "Crying", QUIET => "Quiet")
 
-function createTree(π, number::Array, children::Array, text::Array, observation::Bool)
+# Style
+Sty = Dict(CRYING => "stroke:red", QUIET => "stroke:blue")
+
+function createTree(π, number::Array, children::Array, text::Array, observation::Bool, style::Array)
     depth = number[1]
 	if number[1] == 1
 		o = ""
+        s = ""
 	else
     	o = Obs[observation]
+        s = Sty[observation]
 	end
     push!(children, [])
     push!(text, o*"\n"*Act[π.a])
+    push!(style, s)
     if (length(π.subplans) == 0)
         return
     end
     
     number[1] += 1
     push!(children[depth], number[1])
-    createTree(π.subplans[QUIET], number, children, text, QUIET)
+    createTree(π.subplans[QUIET], number, children, text, QUIET, style)
     number[1] += 1
     push!(children[depth], number[1])
-    createTree(π.subplans[CRYING], number, children, text, CRYING)
+    createTree(π.subplans[CRYING], number, children, text, CRYING, style)
 end
 
-function drawConditionalPlanTree(π, number::Array, children::Array, text::Array, observation::Bool)
-    createTree(π, number, children, text, observation)
+function drawConditionalPlanTree(π, number::Array, children::Array, text::Array, observation::Bool, style)
+    createTree(π, number, children, text, observation, style)
     tree = D3Tree(children, 
         text = text,
+        style = style,
         init_expand = 5)
     return tree
 end
